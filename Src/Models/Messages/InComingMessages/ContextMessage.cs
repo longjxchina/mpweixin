bab62a5af869b673b29d230342c886c588a12416
卢小龙 <longjxchina@qq.com>
2016-06-06 18:35:51 +0800

    添加上下文消息接口

---
+++ Src/Models/Messages/InComingMessages/ContextMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace MpWeiXin.Models.Messages.InComingMessages
+{
+    /// <summary>
+    /// 上下文
+    /// </summary>
+    public class ContextMessage
+    {
+        public string Context { get; set; }
+        public Message Message { get; set; }
+    }
+}
---
+++ Src/Services/IMessageContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+using MpWeiXin.Models.Messages.InComingMessages;
+
+namespace MpWeiXin.Services
+{
+    public interface IMessageContext
+    {
+        /// <summary>
+        /// 添加消息 
+        /// </summary>
+        /// <param name="openId">The open identifier.</param>
+        /// <param name="message">The message.</param>
+        void AddMessage(string openId, ContextMessage message);
+
+        /// <summary>
+        /// 删除消息
+        /// </summary>
+        /// <param name="opendId">The opend identifier.</param>
+        /// <param name="message">The message.</param>
+        void RemoveMessage(string opendId, ContextMessage message);
+
+        /// <summary>
+        /// 获取上下文消息列表
+        /// </summary>
+        /// <param name="openId">The open identifier.</param>
+        /// <param name="context">The context.</param>
+        /// <returns></returns>
+        IList<ContextMessage> GetMessages(string openId, string context);
+    }
+}
